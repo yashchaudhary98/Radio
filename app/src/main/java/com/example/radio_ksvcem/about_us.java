86b90310231dc592bd3b9ac12dcc09902b17e624
yashchaudhary98 <yashchaudharyx20@gmail.com>
2023-02-20 13:16:41 +0530

    added gallery, privacy policy, and about us

--- app/src/main/java/com/example/radio_ksvcem/about_us.java
+++ app/src/main/java/com/example/radio_ksvcem/about_us.java
@@ -1,10 +1,21 @@
 package com.example.radio_ksvcem;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.radio_ksvcem.databinding.ActivityAboutUsBinding;
 
+import java.io.File;
+
 public class about_us extends drawerBase {
+
+    WebView webView;
 
     ActivityAboutUsBinding activityAboutUsBinding;
     @Override
@@ -13,5 +24,29 @@ public class about_us extends drawerBase {
         activityAboutUsBinding = ActivityAboutUsBinding.inflate(getLayoutInflater());
         setContentView(activityAboutUsBinding.getRoot());
         allocateActivityTitle("About Us");
+        webView = findViewById(R.id.aboutus_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://sandesh-radio-about-us.netlify.app/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+
+
+        }
+
+    @Override
+    public void onBackPressed() {
+      if(webView.canGoBack()){
+
+          webView.goBack();
+
+      }else{
+
+          super.onBackPressed();
+      }
     }
 }
+
--- app/src/main/java/com/example/radio_ksvcem/privacy_policy.java
+++ app/src/main/java/com/example/radio_ksvcem/privacy_policy.java
@@ -1,10 +1,17 @@
 package com.example.radio_ksvcem;
 
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.radio_ksvcem.databinding.ActivityPrivacyPolicyBinding;
 
 public class privacy_policy extends drawerBase {
+
+    WebView webView;
+
 
     ActivityPrivacyPolicyBinding activityPrivacyPolicyBinding;
 
@@ -14,5 +21,25 @@ public class privacy_policy extends drawerBase {
         activityPrivacyPolicyBinding = ActivityPrivacyPolicyBinding.inflate(getLayoutInflater());
         setContentView(activityPrivacyPolicyBinding.getRoot());
         allocateActivityTitle("Privacy Policy");
+        webView = findViewById(R.id.privacy_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.freeprivacypolicy.com/live/d500f8fb-bb45-497a-b52d-c335d6caa76a\n");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+    }
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+
+            webView.goBack();
+
+        }else{
+
+            super.onBackPressed();
+        }
     }
 }
